21be40c278e6dcb8a27517c6959ea9393562ac76
allworldg <944937881@qq.com>
2025-08-08 11:28:17 +0800

    init

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
-	logger := getLogger("./log/log.txt")
+	logger := getLogger("/Users/allworldg/Dev/go-lsp/log/log.txt")
 	logger.Println("fine the first log is created")
 	for scanner.Scan() {
 		handleMessage(scanner.Text())
@@ -19,14 +19,12 @@ func main() {
 }
 func handleMessage(_ any) {}
 func getLogger(filePath string) *log.Logger {
-	//createFile
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
 	if err != nil {
 		fmt.Println(err)
 		panic("u do not give a right filePath")
 	}
 	logger := log.New(file, "golsp ", log.Ldate|log.Ltime|log.Lshortfile)
-	//usefile to save log
 	return logger
 
 }
